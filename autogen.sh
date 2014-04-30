ddbf747560745b168f1a8df59cb13b1d5a902535
Sam Kottler <shk@linux.com>
2014-04-30 02:17:27 -0400

    autogen: Require libtool, make error handling more generic

--- autogen.sh
+++ autogen.sh
@@ -6,11 +6,25 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd $srcdir
 
-AUTORECONF=`which autoreconf`
-if test -z $AUTORECONF; then
-        echo "*** No autoreconf found, please intall it ***"
+function missing_tool() {
+        echo "*** No $1 found, please install it ***"
         exit 1
-fi
+}
+
+# This function takes the name of a binary to search for in the system's path
+# as its sole argument. In the event the binary is not found, it will raise
+# a human-parseable error using missing_tool() and exit 1.
+function check_binary_in_path() {
+        TOOL=$1
+
+        CHECK_TOOL=`which $TOOL`
+        if test -z $CHECK_TOOL; then
+                missing_tool $TOOL
+        fi
+}
+
+check_binary_in_path "autoreconf"
+check_binary_in_path "libtool"
 
 mkdir -p m4
 
